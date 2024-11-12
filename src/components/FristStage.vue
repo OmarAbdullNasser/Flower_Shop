49b8dc6455b2e070f16cad119e42345082c560d5
OmarAbdullNasser <65048313+OmarAbdullNasser@users.noreply.github.com>
2024-11-12 22:25:59 +0200

    whatsapp interation Api

---
+++ src/components/FristStage.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="FristStage">
+    <div class="BtnRow d-flex my-5">
+      <button
+        class="btn p-4 BtnSomeOne d-flex flex-column justify-content-center align-items-center"
+        @click="ToggleSender('ToSomeOne')"
+      >
+        <font-awesome-icon
+          icon="fa-solid fa-check"
+          class="my-3"
+          v-if="SenderBox === 'ToSomeOne'"
+        />
+        <font-awesome-icon :icon="['far', 'circle']" v-else class="my-3" />
+        <p class="mb-0">Ship to someone</p>
+      </button>
+
+      <button class="btn BtnMe" @click="ToggleSender('ToMe')">
+        <font-awesome-icon
+          icon="fa-solid fa-check"
+          class="my-3"
+          v-if="SenderBox === 'ToMe'"
+        />
+        <font-awesome-icon :icon="['far', 'circle']" v-else class="my-3" />
+        <p class="mb-0">Ship to me</p>
+      </button>
+    </div>
+    <div class="SenderArea">
+      <ShipToSomeone v-if="SenderBox === 'ToSomeOne'" />
+      <ShipToMe v-else />
+    </div>
+    <div class="NextStage my-3 d-flex flex-column">
+      <span class="my-3">Any Extra Notes </span>
+      <textarea
+        name=""
+        id=""
+        cols="30"
+        rows="5"
+        placeholder="Add your needs.."
+      ></textarea>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ShipToMe from "@/components/shipToMe.vue";
+import ShipToSomeone from "@/components/ShipToSomeone.vue";
+import { ref } from "vue";
+const SenderBox = ref("ToSomeOne");
+const ToggleSender = (to) => {
+  SenderBox.value = to;
+  console.log(SenderBox.value);
+};
+</script>
+
+<style lang="scss" scoped>
+.FristStage {
+  .BtnRow {
+    button {
+      font-size: 18px;
+      font-weight: 700;
+      width: 210px;
+      text-align: center;
+      border: 2px solid #400a3f;
+      background: #400a3f;
+      border-radius: 4px;
+      margin: 0 16px 0 0;
+      transition: 0.1s all ease-in-out;
+      cursor: pointer;
+      color: #fff;
+      height: 120px;
+    }
+  }
+  .NextStage {
+    textarea {
+      resize: none;
+      border-radius: 15px;
+      padding: 15px;
+    }
+  }
+}
+@media screen and (max-width: 991.9px) {
+  .FristStage {
+    .NextStage {
+      button {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/SecandStage.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="SecandStage">
+    <div class="container">
+      <div class="InfoAddress">
+        <label> Recipient Details</label>
+        <div class="name d-flex flex-column">
+          <label for="Name">Name <span class="star">*</span></label>
+          <input type="text" name="Name" id="" />
+        </div>
+
+        <div class="Mobile d-flex flex-column">
+          <label for="Mobile">Mobile <span class="star">*</span></label>
+          <input type="tel" name="Mobile" id="" />
+        </div>
+
+        <div class="Deliver">
+          <label for="Deliver" class="d-block"
+            >Deliver To <span class="star">*</span></label
+          >
+
+          <divc class="options mt-2 d-flex align-items-center">
+            <div class="office d-flex align-content-center">
+              <input
+                type="radio"
+                class="me-2"
+                name="Deliver"
+                value="Office"
+                id=""
+              />
+              <label for="Office">Office</label>
+            </div>
+            <div class="Home d-flex align-content-center">
+              <input
+                type="radio"
+                class="mx-2"
+                name="Deliver"
+                value="Home"
+                id=""
+              />
+              <label for="Home">Home</label>
+            </div>
+          </divc>
+        </div>
+
+        <div class="Area">
+          <label for="Area">Area <span>*</span></label>
+          <select name="Area" id="">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+          </select>
+        </div>
+
+        <div class="Street">
+          <div class="SName">
+            <label for="SName">Street Name <span>*</span></label>
+            <input type="text" name="SName" id="" />
+          </div>
+
+          <div class="Apartment">
+            <label for="Apartment">Apartment No. <span>*</span></label>
+            <input type="text" name="Apartment" id="" />
+          </div>
+
+          <div class="Floor">
+            <label for="Floor ">Floor <span>*</span></label>
+            <input type="text" name="Floor " id="" />
+          </div>
+        </div>
+
+        <div class="Greeting">
+          <label for="Greeting">Greeting Card Message </label>
+          <textarea name="Greeting " id="" cols="30" rows="10"></textarea>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.SecandStage {
+  .InfoAddress {
+    label {
+      font-weight: 400;
+      font-size: 12px;
+      color: #202020;
+      letter-spacing: 0;
+      margin: 15px 0 4px 0;
+    }
+    .star {
+      color: red;
+    }
+    .name,
+    .Mobile {
+      input {
+        border: 1px solid #400a3f;
+        line-height: 35px;
+        padding: 0 16px;
+        border-radius: 4px;
+        font-size: 12px;
+        transition: 0.2s all;
+        background-color: #fefefd;
+        height: auto;
+        width: 80%;
+      }
+    }
+    .Deliver {
+      label:first-child {
+        font-weight: 400;
+        font-size: 12px;
+        color: #202020;
+        letter-spacing: 0;
+        margin: 15px 0 4px 0;
+      }
+      .options {
+        label {
+          color: #400a3f;
+          font-size: 14px;
+          font-weight: 700;
+          margin: 0;
+        }
+        input {
+          accent-color: #400a3f;
+        }
+      }
+    }
+  }
+}
+</style>
